d92041d7d797ba9c603f7a2adde6011ec2386873
Kazu <hlyanw@yahoo.com>
2024-09-23 21:53:25 +0630

    Built stockQuote model (DJ dev)

--- zigzag_game_store/main/models.py
+++ zigzag_game_store/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from PIL import Image
 
-# Create your models here.
+class newStocks(models.Model):
+    image = models.ImageField(upload_to='images/')
+    alt = models.CharField(max_length=10, default="game_account")
+
+    def clean(self):
+        # Ensure you have an image before checking its dimensions
+        if self.image:
+            img = Image.open(self.image)
+            width, height = img.size
+            width_limit, height_limit = 335, 150  # Integer values for pixel dimensions
+
+            # Check if the image exceeds the defined limits
+            if width > width_limit or height > height_limit:
+                raise ValidationError(f"Image size should not exceed {width_limit}px by {height_limit}px.")
+
+    def __str__(self):
+        return self.alt
+
+    
+
+
+class stockQuotes(models.Model):
+    new_stock = models.OneToOneField(newStocks, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, default="Title")
+    text = models.CharField(max_length=200, default="Some description stuff.")
+
+class socialSites(models.Model):
+    ...
+
+
--- zigzag_game_store/main/views.py
+++ zigzag_game_store/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(response):
+    return HttpResponse("<h1>Home Page</h1>")
+
+def shop(response):
+    return HttpResponse("<h1>Shop Page</h1>")
